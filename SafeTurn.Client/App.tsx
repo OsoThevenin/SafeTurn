784640553e4cb3bf0871fd9b5d83c5de99be4eca
Pau Rosello <pau.rosello@itequia.com>
2020-05-01 13:02:59 +0200

    React Native Setup

---
+++ SafeTurn.Client/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>SaveTurn App</Text>
+      <Text>Itequia Covid19</Text>
+      <Button
+        onPress={() => Alert.alert('Has apretat el botó')}
+        title="Botó sense utilitat"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
